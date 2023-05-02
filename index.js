c7ac0f709e3763d348e0fbf1c8fd8a43a2c1e71c
github_username <you@domain.com>
2023-05-02 16:46:17 +0600

    fake data create

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors =require('cors');
+
+
+const app = express();
+app.use(cors());
+
+const allFakeData = require("./fakeData.json");
+
+app.get('/allData', (req, res) => {
+    res.send({allFakeData});
+})
+
+app.listen(5000, () => {
+    console.log('server is running');
+})
