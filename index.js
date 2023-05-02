96c63a979f6978cf80c63d76237297af84a44c8d
github_username <you@domain.com>
2023-05-02 22:06:27 +0600

    details page get

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/allData', (req, res) => {
     console.log('fake data')
     res.send(allFakeData);
 })
+app.get('/allData:id', (req, res) => {
+   const id = req.params.id;
+   console.log(id)
+})
 
 app.listen(5000, () => {
     console.log('server is running');
